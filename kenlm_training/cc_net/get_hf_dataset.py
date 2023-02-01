cad1dc1cc833fbb95919f61f943ca8cbfc827838
Siddhesh <sid1793@gmail.com>
2023-02-01 00:49:35 -0800

    works now

--- kenlm_training/cc_net/get_hf_dataset.py
+++ kenlm_training/cc_net/get_hf_dataset.py
@@ -1,3 +1,6 @@
+import cld3
+import random
+import nltk
 from typing import Optional
 
 import func_argparse
@@ -6,7 +9,12 @@ from tqdm import tqdm
 
 from cc_net import text_normalizer
 
-def text_normalizer_list(text_list, accent, case, numbers, punct):
+sent_tokenizer = nltk.data.load("tokenizers/punkt/PY3/english.pickle")
+
+random.seed(1234)
+
+def text_normalizer_list(text, accent, case, numbers, punct):
+    text_list = sent_tokenizer.tokenize(text)
     norm_text_list = []
     for text in text_list:
         norm_text_list.append(text_normalizer.normalize(text, accent=accent, case=case, numbers=numbers, punct=punct))
@@ -19,7 +27,7 @@ def dl(
     data_dir: Optional[str] = None,
     data_files: Optional[str] = None,
     split: Optional[str] = None,
-    streaming: bool = False,
+    streaming: bool = True,
     accent: bool = False,
     case: bool = False,
     numbers: bool = True,
@@ -27,18 +35,18 @@ def dl(
     max_docs: Optional[int] = None,
     seed: int = 0,
     buffer_size: int = 10000,
-    text_key: str = "TEXT"
+    text_key: str = "TEXT",
+    buffer_shuffle: bool = False
 ):
     """Download dataset from the Hugging Face hub."""
     dataset = load_dataset(
-        dataset,
-        name=name,
-        data_dir=data_dir,
-        data_files=data_files,
-        split=split,
-        streaming=streaming,
+       dataset,
+       name=name,
+       data_dir=data_dir,
+       data_files=data_files,
+       split="train",
+       streaming=streaming,
     )
-
     norm_text_key = text_key + "_norm"
     dataset_norm = dataset.map(
         lambda x: {norm_text_key : text_normalizer_list(
@@ -46,8 +54,9 @@ def dl(
         )}
     )
 
-    print (next(iter(dataset_norm)).keys())
-    dataset_norm = dataset_norm.shuffle(seed=seed)
+    if buffer_shuffle:
+        dataset_norm = dataset_norm.shuffle(buffer_size=buffer_size, seed=seed)
+
     count = 0
     with open(output_file, "w") as o:
         with tqdm(total=max_docs) as pbar:
@@ -67,6 +76,27 @@ def dl(
                 if max_docs and count == max_docs:
                     break
 
+    # shuffle the sentences later
+    with open(output_file, "r") as f:
+        lines = f.readlines()
+
+    # filter lines
+    print ("Filtering lines for english")
+    filtered_lines = []
+    for line in tqdm(lines):
+        lang = cld3.get_language(line).language
+
+        if lang == 'en':
+            filtered_lines.append(line)
+
+    # shuffle and write to disk
+    print ("Shuffle and write to disk")
+    random.shuffle(filtered_lines)
+
+    with open(output_file, "w") as f:
+        for line in tqdm(filtered_lines):
+            line = line.rstrip("\n")
+            print (line, file=f)
 
 if __name__ == "__main__":
     func_argparse.main(dl)
